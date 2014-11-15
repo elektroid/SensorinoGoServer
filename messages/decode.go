c8f56c2bb3ee7c76c755634305379b65da9a7ee3
elektroid <elektroid@nobodix.org>
2014-11-15 02:53:48 +0100

    decode ok

--- messages/decode.go
+++ messages/decode.go
@@ -9,12 +9,20 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"strings"
+)
+
+const (
+	Publish = iota
+	Request = iota
+	Set     = iota
+	Error   = iota
 )
 
 type BaseMessage struct {
 	From      string   `json:"from"`
 	To        string   `json:"to"`
-	Type      string   `json:"type"`
+	Type      int64    `json:"type"`
 	ServiceId []int64  `json:"serviceId"`
 	DataType  []string `json:"dataType"`
 	Count     []int64  `json:"count"`
@@ -32,8 +40,9 @@ type BaseMessage struct {
 
 func DecodeMessage(jsonMsg string) (*BaseMessage, error) {
 	b := &BaseMessage{}
-	b.DataType = []string{}
-	b.Count = []int64{}
+	//	b.DataType = []string{}
+	//	b.Count = []int64{}
+
 	var rawData map[string]interface{}
 	if err := json.Unmarshal([]byte(jsonMsg), &rawData); err != nil {
 		return nil, err
@@ -65,49 +74,101 @@ func DecodeMessage(jsonMsg string) (*BaseMessage, error) {
 	if !ok {
 		return nil, errors.New("No type in base message")
 	}
-	b.Type = mType.(string)
+	switch strings.ToUpper(mType.(string)) {
+	case "PUBLISH":
+		b.Type = Publish
+	case "REQUEST":
+		b.Type = Request
+	case "SET":
+		b.Type = Set
+	case "ERROR":
+		b.Type = Error
+	default:
+		return nil, errors.New(fmt.Sprintf("Invalid type in base message:%s", mType.(string)))
+	}
 	delete(rawData, "type")
 
 	sIds, ok := rawData["serviceId"]
-	value := reflect.ValueOf(sIds)
-	fmt.Printf("%+v\n", sIds)
-	fmt.Println(value.Kind())
 	if ok {
-		switch value.Kind() {
-		case reflect.Float32, reflect.Float64:
-			b.ServiceId = []int64{int64(value.Float())}
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			b.ServiceId = []int64{int64(value.Int())}
-		case reflect.Slice:
-			//we want either ints, floats or strings
-			b.ServiceId = make([]int64, value.Len())
-			for i := 0; i < value.Len(); i++ {
-
-				//fmt.Println(value.Index(0).Int())
-				switch value.Index(i).Elem().Kind() {
-				case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-					b.ServiceId[i] = int64(value.Index(i).Elem().Int())
-				case reflect.String:
-					v, err := strconv.ParseInt(value.Index(i).Elem().String(), 10, 64)
-					if err != nil {
-						return nil, errors.New("Failed to decode service id (atoi)")
-					}
-					b.ServiceId[i] = v
-				}
-				//	fmt.Printf("+%v\n", value.Index(i).Elem().Kind())
-			}
-
-		default:
-			return nil, errors.New("Failed to decode service type, unexpected type")
+		var err error
+		b.ServiceId, err = extractInt64Array(reflect.ValueOf(sIds))
+		if err != nil {
+			return nil, err
 		}
 	}
-
 	// could check type to see if it's normal not to find it
 	delete(rawData, "serviceId")
 
-	b.Data = rawData
+	dataType, ok := rawData["dataType"]
+	if ok {
+		value := reflect.ValueOf(dataType)
+		switch value.Kind() {
+		case reflect.String:
+			b.DataType = []string{dataType.(string)}
+		case reflect.Slice:
+			for i := 0; i < value.Len(); i++ {
+				if value.Index(i).Elem().Kind() == reflect.String {
+					b.DataType = append(b.DataType, value.Index(i).Elem().String())
+				}
+			}
+		}
+	}
+	delete(rawData, "dataType")
+
+	count, ok := rawData["count"]
+	if ok {
+		var err error
+		b.Count, err = extractInt64Array(reflect.ValueOf(count))
+		if err != nil {
+			return nil, err
+		}
+	}
+	delete(rawData, "count")
+
+	if len(rawData) > 0 {
+		b.Data = rawData
+	}
 
 	return b, nil
+
+}
+
+func extractInt64Array(value reflect.Value) ([]int64, error) {
+
+	var array []int64
+
+	switch value.Kind() {
+	case reflect.Float32, reflect.Float64:
+		array = []int64{int64(value.Float())}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		array = []int64{int64(value.Int())}
+	case reflect.Slice:
+		//we want either ints, floats or strings
+		array = make([]int64, value.Len())
+		for i := 0; i < value.Len(); i++ {
+
+			switch value.Index(i).Elem().Kind() {
+
+			case reflect.Float32, reflect.Float64:
+				array[i] = int64(value.Index(i).Elem().Float())
+			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				array[i] = int64(value.Index(i).Elem().Int())
+			case reflect.String:
+				v, err := strconv.ParseInt(value.Index(i).Elem().String(), 10, 64)
+				if err != nil {
+					return nil, errors.New("Failed to decode service id (atoi)")
+				}
+				array[i] = v
+			default:
+				return nil, errors.New(fmt.Sprintf("unable to decode int : +%v", value.Index(i).Elem()))
+			}
+		}
+
+	default:
+		return nil, errors.New(fmt.Sprintf("Failed to decode int array, unexpected ", value.Kind()))
+	}
+
+	return array, nil
 
 }
 
--- messages/decode_test.go
+++ messages/decode_test.go
@@ -15,11 +15,21 @@ type testDecode struct {
 var set = []testDecode{
 	testDecode{
 		`{ "from": "0", "to": "10", "type": "request", "serviceId": 0 }`,
-		&messages.BaseMessage{From: "0", To: "10", Type: "request", ServiceId: []int64{0}},
+		&messages.BaseMessage{From: "0", To: "10", Type: messages.Request, ServiceId: []int64{0}},
 	},
 	testDecode{
 		`{ "from": "1.2.3.4", "to": "10", "type": "request", "serviceId": [ 0, "1", "2" ] }`,
-		&messages.BaseMessage{From: "1.2.3.4", To: "10", Type: "request", ServiceId: []int64{0, 1, 2}},
+		&messages.BaseMessage{From: "1.2.3.4", To: "10", Type: messages.Request, ServiceId: []int64{0, 1, 2}},
+	},
+
+	testDecode{
+		`{ "from": "1.2.3.4", "to": "10", "type": "publish", "serviceId":  2,  "switch": false  }`,
+		&messages.BaseMessage{From: "1.2.3.4", To: "10", Type: messages.Publish, ServiceId: []int64{2}, Data: map[string]interface{}{"switch": false}},
+	},
+
+	testDecode{
+		`{ "from": "10.2", "to": "0", "type": "publish", "serviceId": 2, "dataType": "Switch", "count": [ 0, 1 ] }`,
+		&messages.BaseMessage{From: "10.2", To: "0", Type: messages.Publish, DataType: []string{"Switch"}, ServiceId: []int64{2}, Count: []int64{0, 1}},
 	},
 }
 
@@ -33,11 +43,12 @@ func TestDecode(t *testing.T) {
 		}
 		if reflect.DeepEqual(b, pair.Msg) == false {
 			failures++
-			t.Logf("decoded does not produce proper result:\n %+v\n %+v", pair.Msg, b)
+			t.Logf("decoded does not produce proper result:\njson:%s\n %+v\n %+v", pair.Json, pair.Msg, b)
+			j2, _ := json.Marshal(pair.Msg)
+			t.Log(string(j2))
+
 			j, _ := json.Marshal(b)
 			t.Log(string(j))
-			j2, _ := json.Marshal(b)
-			t.Log(string(j2))
 
 		}
 	}
